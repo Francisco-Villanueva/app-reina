ae1d28ac971ec6e7e9d42de6b759cf594fe11882
unknown <franciscovillanuevaj99@gmail.com>
2022-05-13 01:22:53 -0300

    fourth commit

--- index.js
+++ index.js
@@ -26,6 +26,9 @@ const burgers = [
 var cantBlue=document.getElementById('cantB');
 var cantClasic=document.getElementById('cantClas');
 var cantCuarto=document.getElementById('cantClib');
+var cantBacon=document.getElementById('cantBacon');
+var cantCh=document.getElementById('cantCh');
+var cantLife=document.getElementById('cantLife');
 
 var precioFinal=0;
 var cantidadPedidos=0;
@@ -76,10 +79,10 @@ function agregarClib(){
         newCell = newRow.insertCell(1);
         newCell.textContent = 'Cuarto de libra';
         newCell = newRow.insertCell(2);
-        newCell.textContent ='$ '+ 950 * parseInt(cantCuarto.value);
+        newCell.textContent ='$ '+ 900 * parseInt(cantCuarto.value);
         
         //precioDeVenta +=  menu[i].precio * parseInt(cant.value);
-        precioFinal += 950 * parseInt(cantCuarto.value);
+        precioFinal += 900 * parseInt(cantCuarto.value);
         cantidadPedidos += parseInt(cantCuarto.value);
         cantCuarto.value='';
     }
@@ -104,12 +107,93 @@ function agregarClas(){
         newCell = newRow.insertCell(1);
         newCell.textContent = 'Clásica';
         newCell = newRow.insertCell(2);
-        newCell.textContent ='$ '+ 950 * parseInt(cantClasic.value);
+        newCell.textContent ='$ '+ 900 * parseInt(cantClasic.value);
         
         //precioDeVenta +=  menu[i].precio * parseInt(cant.value);
-        precioFinal += 950 * parseInt(cantClasic.value);
+        precioFinal += 900 * parseInt(cantClasic.value);
         cantidadPedidos += parseInt(cantClasic.value);
         cantClasic.value='';
+    }
+}
+function agregarCh(){
+    if(cantCh.value===''){
+        swal("Falta la cantidad!!", "Aclará la cantidad de burguers " ,"warning", {
+            button: "OK",
+        });
+    } else if(parseInt(cantCh.value)<0){
+        swal("puse menor a cero", "Aclará la cantidad de burguers " ,"warning", {
+            button: "OK",
+        });
+    }
+    
+    else{
+        let tableRef = document.getElementById("tablaprueba");
+        let newRow = tableRef.insertRow(-1);
+        let newCell = newRow.insertCell(0);
+        newCell.textContent = cantCh.value;
+        newCell = newRow.insertCell(1);
+        newCell.textContent = 'Cheese';
+        newCell = newRow.insertCell(2);
+        newCell.textContent ='$ '+ 850 * parseInt(cantCh.value);
+        
+        //precioDeVenta +=  menu[i].precio * parseInt(cant.value);
+        precioFinal += 850 * parseInt(cantCh.value);
+        cantidadPedidos += parseInt(cantCh.value);
+        cantCh.value='';
+    }
+}
+function agregarBacon(){
+    if(cantBacon.value===''){
+        swal("Falta la cantidad!!", "Aclará la cantidad de burguers " ,"warning", {
+            button: "OK",
+        });
+    } else if(parseInt(cantBacon.value)<0){
+        swal("puse menor a cero", "Aclará la cantidad de burguers " ,"warning", {
+            button: "OK",
+        });
+    }
+    
+    else{
+        let tableRef = document.getElementById("tablaprueba");
+        let newRow = tableRef.insertRow(-1);
+        let newCell = newRow.insertCell(0);
+        newCell.textContent = cantBacon.value;
+        newCell = newRow.insertCell(1);
+        newCell.textContent = 'Bacon';
+        newCell = newRow.insertCell(2);
+        newCell.textContent ='$ '+ 900 * parseInt(cantBacon.value);
+        
+        //precioDeVenta +=  menu[i].precio * parseInt(cant.value);
+        precioFinal += 900 * parseInt(cantBacon.value);
+        cantidadPedidos += parseInt(cantBacon.value);
+        cantBacon.value='';
+    }
+}
+function agregarLife(){
+    if(cantLife.value===''){
+        swal("Falta la cantidad!!", "Aclará la cantidad de burguers " ,"warning", {
+            button: "OK",
+        });
+    } else if(parseInt(cantLife.value)<0){
+        swal("puse menor a cero", "Aclará la cantidad de burguers " ,"warning", {
+            button: "OK",
+        });
+    }
+    
+    else{
+        let tableRef = document.getElementById("tablaprueba");
+        let newRow = tableRef.insertRow(-1);
+        let newCell = newRow.insertCell(0);
+        newCell.textContent = cantLife.value;
+        newCell = newRow.insertCell(1);
+        newCell.textContent = 'Life';
+        newCell = newRow.insertCell(2);
+        newCell.textContent ='$ '+ 950 * parseInt(cantLife.value);
+        
+        //precioDeVenta +=  menu[i].precio * parseInt(cant.value);
+        precioFinal += 950 * parseInt(cantLife.value);
+        cantidadPedidos += parseInt(cantLife.value);
+        cantLife.value='';
     }
 }
 
